65999addd2d90c2ee73ba77d0b7e025dc3d1a817
avengerstark <avengerstark99@gmail.com>
2016-07-25 12:11:18 +0600

    Add JS and AJAX

---
+++ JS and AJAX/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JS_and_AJAX.Models;
+
+namespace JS_and_AJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookContext _db = new BookContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult BookSearch(string name)
+        {
+            var allBooks = _db.Books.Where(a => a.Author.Contains(name)).ToList();
+            if (allBooks.Count <= 0)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(allBooks);
+           
+        }
+
+    }
+}
---
+++ JS and AJAX/Models/BookContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace JS_and_AJAX.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext() : base("BookStoreDb") { }
+
+        public DbSet<Book> Books { get; set; }
+
+        static BookContext()
+        {
+            Database.SetInitializer<BookContext>(new BookInitializer());
+        }
+    }
+
+
+    class BookInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            Book book1 = new Book { Name = "Три товарища", Author = "Ремарк", Price = 3000 };
+            Book book2 = new Book { Name = "1984", Author = "Джордж Оруэлл", Price = 2000 };
+            Book book3 = new Book { Name = "Sea wolf", Author = "Jack London", Price = 4000 };
+            Book book4 = new Book { Name = "Триумфальная арка", Author = "Ремарк", Price = 3000 };
+            Book book5 = new Book { Name = "Время жить и время умирать", Author = "Ремарк", Price = 3000 };
+
+            context.Books.AddRange(new List<Book> { book1, book2, book3, book4, book5 });
+            context.SaveChanges();
+        }
+    }
+
+}
